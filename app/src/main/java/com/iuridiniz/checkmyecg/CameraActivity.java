f1745222fd2466cd4c510722258e203789a75eab
Iuri Diniz <iuridiniz@gmail.com>
2015-02-28 17:39:33 -0300

    Implementation of contrast by curve (faster than normalize)

--- app/src/main/java/com/iuridiniz/checkmyecg/CameraActivity.java
+++ app/src/main/java/com/iuridiniz/checkmyecg/CameraActivity.java
@@ -6,12 +6,15 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 
+import org.apache.commons.math3.analysis.UnivariateFunction;
+import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
 import org.opencv.imgproc.Imgproc;
 
 interface Filter {
@@ -68,6 +71,33 @@ class NormalizerFilter implements Filter {
 
     public void setNormType(int mNormType) {
         this.mNormType = mNormType;
+    }
+}
+
+class ContrastFilter implements Filter {
+    private final Mat mRgbaDst;
+    private final Mat mLut;
+
+    public ContrastFilter(int rows, int cols, int mMin, int mMax) {
+        mRgbaDst = new Mat(rows, cols, CvType.CV_8UC4);
+        mLut = new MatOfInt();
+
+        SplineInterpolator si = new SplineInterpolator();
+        UnivariateFunction f = si.interpolate(new double[] {0, mMin, mMax, 255},
+                                              new double[] {0, 0, 255, 255});
+        /* create my mLut */
+        mLut.create(256, 1, CvType.CV_8UC4);
+
+        for (int i = 0; i < 256; i++) {
+            final double v = f.value(i); /* r, g, b */
+            mLut.put(i, 0, v, v, v, i); /* alpha not touched */
+        }
+    }
+
+    @Override
+    public Mat apply(Mat src) {
+        Core.LUT(src, mLut, mRgbaDst);
+        return mRgbaDst;
     }
 }
 
