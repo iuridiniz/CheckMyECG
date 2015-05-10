8fd7454858366fd4aae20429419b72d8de0edbf6
Iuri Diniz <iuridiniz@gmail.com>
2015-05-10 09:23:02 -0300

    initing opencv on Result Activity

--- app/src/main/java/com/iuridiniz/checkmyecg/ResultEkgActivity.java
+++ app/src/main/java/com/iuridiniz/checkmyecg/ResultEkgActivity.java
@@ -6,10 +6,14 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.OpenCVLoader;
 
 public class ResultEkgActivity extends ActionBarActivity {
 
@@ -17,15 +21,17 @@ public class ResultEkgActivity extends ActionBarActivity {
             "com.iuridiniz.checkmyecg.ResultEkgActivity.extra.PHOTO_URI;";
     public static final String EXTRA_PHOTO_DATA_PATH =
             "com.iuridiniz.checkmyecg.ResultEkgActivity.extra.DATA_PATH;";
+    private static final String TAG = "ResultEkg";
 
     private ShareActionProvider mShareActionProvider;
     private Uri mUri;
     private String mDataPath;
     private ImageView mImageContent;
     private TextView mTextContent;
+    private boolean mOpenCvLoaded = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_ekg);
 
@@ -36,9 +42,42 @@ public class ResultEkgActivity extends ActionBarActivity {
         mImageContent = (ImageView) findViewById(R.id.result_image);
         mTextContent = (TextView) findViewById(R.id.result_text);
 
-        mTextContent.setText(R.string.text_ekg_fail);
+        /* init openCV */
+        if (OpenCVLoader.initDebug()) {
+            Log.i(TAG, "OpenCV loaded successfully (static initialization)");
+            mOpenCvLoaded = true;
+            onReady(savedInstanceState);
+            return;
+        }
 
+        /* binaries not included, use OpenCV manager */
+        Log.i(TAG, "OpenCV libs not included on APK, trying async initialization");
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_10, this, new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case BaseLoaderCallback.SUCCESS:
+                        Log.i(TAG, "OpenCV loaded successfully (async initialization)");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mOpenCvLoaded = true;
+                                onReady(savedInstanceState);
+                            }
+                        });
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        });
+
+    }
+
+    private void onReady(Bundle savedInstanceState) {
         mImageContent.setImageURI(mUri);
+        mTextContent.setText(R.string.text_ekg_fail);
     }
 
     @Override
