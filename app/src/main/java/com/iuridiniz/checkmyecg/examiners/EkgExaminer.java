fb126c5f1ca1aae04c749704ff5d7da47ddf6021
Iuri Diniz <iuridiniz@gmail.com>
2015-12-01 13:33:08 -0300

    peak detection (attempt to implement)

--- app/src/main/java/com/iuridiniz/checkmyecg/examiners/EkgExaminer.java
+++ app/src/main/java/com/iuridiniz/checkmyecg/examiners/EkgExaminer.java
@@ -1,9 +1,10 @@
 package com.iuridiniz.checkmyecg.examiners;
 
-import android.util.Log;
+//import android.util.Log;
 
 import org.apache.commons.math3.stat.StatUtils;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,16 +17,24 @@ public class EkgExaminer {
     private static final String TAG = "EkgExaminer";
     private final double[] time;
     private final double[] voltage;
+    private int[] peaks;
+    private int[] depressions;
+    private LinkedList<Integer> __peaksPositions = null;
+    private double voltageSignificancy;
+
+    public EkgExaminer(double[] signalX, double[] signalY) {
+        this(signalX, signalY, Orientation.DOWN);
+    }
 
     private TreeMap<Integer, LinkedList<Integer>> getMapVoltage() {
         if (__mapVoltage.size() == 0) {
             /* try to be lazy */
             double max_voltage = StatUtils.max(voltage);
             double min_voltage = StatUtils.min(voltage);
-            Log.d(TAG, String.format("max_voltage: %.4f", max_voltage));
-            Log.d(TAG, String.format("min_voltage: %.4f", min_voltage));
+            //Log.d(TAG, String.format("max_voltage: %.4f", max_voltage));
+            //Log.d(TAG, String.format("min_voltage: %.4f", min_voltage));
             for (int i = 0; i < voltage.length; i++) {
-                int key = (int) (voltage[i] * (1/VOLTAGE_RESOLUTION));
+                int key = (int) (voltage[i] * (1/ DEFAULT_VOLTAGE_RESOLUTION));
                 List l = __mapVoltage.get(key);
                 if (l == null) {
                     l = new LinkedList<Integer>();
@@ -44,14 +53,91 @@ public class EkgExaminer {
     private final TreeMap<Integer, LinkedList<Integer>> __mapVoltage;
     private final TreeMap<Integer, LinkedList<Integer>> mapTime;
 
+    boolean storeOnTemp = false;
+    public final LinkedList<Integer> getPeaksPositions() {
+        if (__peaksPositions != null)
+            return __peaksPositions;
+        /* parse peaks */
+        __peaksPositions = new LinkedList<Integer>();
+        int direction = 0;
+        if (time.length < 3) {
+            return __peaksPositions;
+        }
+        LinkedList<Integer> tempSerie = new LinkedList<Integer>();
+
+        for (int i = 0; i < time.length; i++) {
+            int new_direction;
+
+            double volt_diff;
+            if (i == 0) {
+                /* fisrt */
+            }
+
+            if (i+1 == time.length) {
+                /* last */
+                if (direction == 1) {
+                    int pos = i;
+                    if (tempSerie.size() > 0) {
+                        pos = (i - tempSerie.getFirst())/2 + tempSerie.getFirst();
+                    }
+                    __peaksPositions.add(pos);
+                }
+                continue;
+            }
+            volt_diff = voltage[i+1] - voltage[i];
+            if (Math.abs(volt_diff) < this.voltageSignificancy) {
+                new_direction = 0;
+                tempSerie.add(i);
+                //storeOnTemp = true;
+            } else {
+
+                if (volt_diff < 0 ) {
+                    new_direction = -1;
+                } else {
+                    new_direction = 1;
+                }
+                if (new_direction == direction) {
+                    tempSerie.clear();
+                }
+            }
+            boolean flush = false;
+            if (new_direction != 0 && new_direction != direction) {
+                flush=true;
+            }
+
+            if (flush) {
+                tempSerie.add(i);
+                if (new_direction == 1) {
+                    /* depression */
+                } else if (new_direction == -1){
+                    /* peak */
+                    int pos = (tempSerie.getLast()- tempSerie.getFirst())/2 + tempSerie.getFirst() ;
+                    __peaksPositions.add(pos);
+                    //__peaksPositions.add(i);
+                }
+                direction = new_direction;
+                tempSerie.clear();
+                storeOnTemp = false;
+            }
+
+        }
+        return __peaksPositions;
+    }
+
+    public void setVoltageSignificancy(double voltageSignificancy) {
+        this.voltageSignificancy = voltageSignificancy;
+        this.__peaksPositions = null;
+    }
+
+
     public enum Orientation {
         UP, DOWN
     };
     /* 0.1v  is the equivalent of little square in voltage axis */
     /* 0.04s is the equivalent of little square in time axis */
-    public static double VOLTAGE_RESOLUTION = 0.05; /* half of a square */
-    public static double VOLTAGE_SIGNIFICANCY = 0.1; /* one little square */
-    public static double TIME_SIGNIFICANCY = 0.04; /* one little square */
+    public static double DEFAULT_VOLTAGE_RESOLUTION = 0.05; /* half of a square */
+    public static double DEFAULT_VOLTAGE_SIGNIFICANCY = 0.1; /* one little square */
+    public static double DEFAULT_TIME_SIGNIFICANCY = 0.04; /* one little square */
 
     private final Orientation normalOrientation;
 
@@ -65,7 +151,10 @@ public class EkgExaminer {
 
         this.__mapVoltage = new TreeMap<>();
         this.mapTime = new TreeMap<>();
+
+        this.voltageSignificancy = this.DEFAULT_VOLTAGE_SIGNIFICANCY;
     }
+
 
     public double getFrequency() {
 
@@ -76,14 +165,14 @@ public class EkgExaminer {
         /* start from bottom values */
         /* FIXME: this algorithm does not catch two peeks with a little peak between them*/
         key = getMapVoltage().firstKey();
-        while (diff_time < TIME_SIGNIFICANCY && key != null) {
+        while (diff_time < DEFAULT_TIME_SIGNIFICANCY && key != null) {
             List<Integer> list = getMapVoltage().get(key);
             Iterator<Integer> it = list.iterator();
-            while (it.hasNext() && diff_time < TIME_SIGNIFICANCY) {
+            while (it.hasNext() && diff_time < DEFAULT_TIME_SIGNIFICANCY) {
                 int v = it.next();
                 double cur_time = this.time[v];
                 double cur_voltage = this.voltage[v];
-                Log.d(TAG, String.format("Voltage: %.4f at %.4f", cur_voltage, cur_time));
+                //Log.d(TAG, String.format("Voltage: %.4f at %.4f", cur_voltage, cur_time));
                 if (last_time != null) {
                     diff_time = Math.abs(cur_time - last_time);
                 }
@@ -95,7 +184,7 @@ public class EkgExaminer {
         }
         double bottom_frequency = 0.0;
         if (diff_time > 0) {
-            Log.d(TAG, String.format("Bottom difference: %.4f", diff_time));
+            //Log.d(TAG, String.format("Bottom difference: %.4f", diff_time));
 
             bottom_frequency = 60/diff_time;
         }
